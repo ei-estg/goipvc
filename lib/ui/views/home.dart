5d462a9e86be8694110525bfbb0c371a0ee65372
Pedro Cunha <pedro10cunha@gmail.com>
2023-09-15 18:55:37 +0100

    Cleaned HomeView a bit

--- lib/ui/views/home.dart
+++ lib/ui/views/home.dart
@@ -1,15 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:myipvc_budget_flutter/models/myipvc_lesson.dart';
 import 'package:myipvc_budget_flutter/models/myipvc_user.dart';
 import 'package:myipvc_budget_flutter/providers/profile_provider.dart';
-import 'package:myipvc_budget_flutter/providers/schedule_provider.dart';
-import 'package:myipvc_budget_flutter/services/date_verification.dart';
 import 'package:myipvc_budget_flutter/ui/views/error.dart';
-import 'package:myipvc_budget_flutter/ui/views/loading.dart';
-import 'package:myipvc_budget_flutter/ui/widgets/lesson_card.dart';
 import 'package:myipvc_budget_flutter/ui/widgets/profile_picture.dart';
+import 'package:myipvc_budget_flutter/ui/widgets/schedule_tab.dart';
 
 class HomeView extends StatefulWidget {
   const HomeView({Key? key}) : super(key: key);
@@ -39,168 +35,129 @@ class HomeViewState extends State<HomeView> with SingleTickerProviderStateMixin 
     return Consumer(
       builder: (context, ref, child) {
         MyIPVCUser? profile = ref.watch(profileProvider);
-        AsyncValue<List<MyIPVCLesson>> schedule = ref.watch(scheduleProvider);
 
-        return profile != null
-        ? Center(
-            child: Column(
-              children: [
-                Padding(
-                  padding: const EdgeInsets.fromLTRB(0, 20, 0, 20),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceAround,
-                    crossAxisAlignment: CrossAxisAlignment.center,
+      return profile != null
+      ? Center(
+        child: Column(
+          children: [
+            Padding(
+              padding: const EdgeInsets.fromLTRB(0, 20, 0, 20),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
                     children: [
-                      Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          RichText(
-                            text: TextSpan(
+                      RichText(
+                        text: TextSpan(
+                          style: TextStyle(
+                            fontSize: 15,
+                            color: Theme.of(context).colorScheme.onPrimaryContainer,
+                          ),
+                          children: const <TextSpan>[
+                            TextSpan(
+                              text: 'O ',
+                            ),
+                            TextSpan(
+                              text: 'teu',
                               style: TextStyle(
-                                fontSize: 15,
-                                color: Theme.of(context).colorScheme.onPrimaryContainer,
+                                fontWeight: FontWeight.bold,
                               ),
-                              children: const <TextSpan>[
-                                TextSpan(
-                                  text: 'O ',
-                                ),
-                                TextSpan(
-                                  text: 'teu',
-                                  style: TextStyle(
-                                    fontWeight: FontWeight.bold,
-                                  ),
-                                ),
-                                TextSpan(
-                                  text: '.',
-                                  style: TextStyle(
-                                    fontSize: 80,
-                                    height: 0.1
-                                  ),
-                                ),
-                                TextSpan(
-                                  text: 'de partida',
-                                ),
-                              ],
                             ),
-                            textAlign: TextAlign.left,
-                          ),
-                          Text(
-                            "Olá ${profile.nome.split(" ")[0]}!",
-                            style: TextStyle(
-                              fontSize: 32,
-                              color: Theme.of(context).colorScheme.primary,
+                            TextSpan(
+                              text: '.',
+                              style: TextStyle(
+                                fontSize: 80,
+                                height: 0.1
+                              ),
                             ),
-                            textAlign: TextAlign.left,
-                          )
-                        ],
-                      ),
-                      ProfilePicture(
-                        imageData: profile.fotografia,
-                        size: 100,
-                      ),
-                    ],
-                  ),
-                ),
-                IntrinsicHeight(
-                  child: Row(
-                    crossAxisAlignment: CrossAxisAlignment.stretch,
-                    children: [
-                      Expanded(
-                        child:
-                          SvgPicture.asset(
-                            'assets/ipvc-divider.svg',
-                            width: 1000,
-                          ),
-                      ),
-                    ]
-                  )
-                ),
-                Expanded(
-                    child: Column(
-                      children: <Widget>[
-                        TabBar(
-                          controller: _tabController,
-                          tabs: const <Tab>[
-                            Tab(
-                              text: "Horarios",
-                              icon: Icon(Icons.schedule), // Icon for Horarios
-                            ),
-                            Tab(
-                              text: "Ementas",
-                              icon: Icon(Icons.restaurant_menu), // Icon for Ementas
-                            ),
-                            Tab(
-                              text: "Exames",
-                              icon: Icon(Icons.assignment), // Icon for Exames
+                            TextSpan(
+                              text: 'de partida',
                             ),
                           ],
                         ),
-                        Expanded(
-                              child: Card(
-                                elevation: 2,
-                                margin: const EdgeInsets.all(16.0),
-                                child: TabBarView(
-                                  controller: _tabController,
-                                  children: <Widget>[
-                                    Align(
-                                      alignment: Alignment.center,
-                                      child: schedule.when(
-                                          loading: () => const LoadingView(),
-                                          error: (err, stack) => ErrorView(error: "$err"),
-                                          data: (schedule) {
-                                            schedule.removeWhere((lesson) =>
-                                                verifyIfLessonExpired(lesson));
-
-                                            if(schedule.isEmpty){
-                                              return const Center(
-                                                child: Column(
-                                                  mainAxisAlignment: MainAxisAlignment.center,
-                                                  children: [Text("Não existem aulas hoje")],
-                                                ),
-                                              );
-                                            }
-
-                                            return Column(
-                                              children: [
-                                                const ListTile(
-                                                  title: Text(
-                                                      'Hoje:',
-                                                      style: TextStyle(fontSize: 20)
-                                                  ),
-                                                ),
-                                                Expanded(
-                                                  child: ListView(
-                                                    children: [
-                                                      for(var lesson in schedule)
-                                                        LessonCard(lesson: lesson)
-                                                    ],
-                                                  ),
-                                                ),
-                                              ],
-                                            );
-                                          }
-                                      ),
-                                    ),
-                                    const Align(
-                                      alignment: Alignment.center,
-                                      child: Text("Não Existe Ementas Hoje"),
-                                    ),
-                                    const Align(
-                                      alignment: Alignment.center,
-                                      child: Text("Não Existe Exames Hoje"),
-                                    ),
-                                  ],
-                                )
-
-                              )
-                          )
+                        textAlign: TextAlign.left,
+                      ),
+                      Text(
+                        "Olá ${profile.nome.split(" ")[0]}!",
+                        style: TextStyle(
+                          fontSize: 32,
+                          color: Theme.of(context).colorScheme.primary,
+                        ),
+                        textAlign: TextAlign.left,
+                      )
+                    ],
+                  ),
+                  ProfilePicture(
+                    imageData: profile.fotografia,
+                    size: 100,
+                  ),
+                ],
+              ),
+            ),
+            IntrinsicHeight(
+              child: Row(
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: [
+                  Expanded(
+                    child:
+                      SvgPicture.asset(
+                        'assets/ipvc-divider.svg',
+                        width: 1000,
+                      ),
+                  ),
+                ]
+              )
+            ),
+            Expanded(
+                child: Column(
+                  children: <Widget>[
+                    TabBar(
+                      controller: _tabController,
+                      tabs: const <Tab>[
+                        Tab(
+                          text: "Horarios",
+                          icon: Icon(Icons.schedule),
+                        ),
+                        Tab(
+                          text: "Ementas",
+                          icon: Icon(Icons.restaurant_menu),
+                        ),
+                        Tab(
+                          text: "Exames",
+                          icon: Icon(Icons.assignment),
+                        ),
                       ],
-                    )
+                    ),
+                    Expanded(
+                          child: Card(
+                          elevation: 2,
+                          margin: const EdgeInsets.all(16.0),
+                            child: TabBarView(
+                            controller: _tabController,
+                            children: const <Widget>[
+                              ScheduleTab(),
+                              Align(
+                                alignment: Alignment.center,
+                                child: Text("Não Existe Ementas Hoje"),
+                              ),
+                              Align(
+                                alignment: Alignment.center,
+                                child: Text("Não Existe Exames Hoje"),
+                              ),
+                            ],
+                          )
+
+                        )
+                      )
+                  ],
                 )
-              ],
-            )
-          )
-        : const ErrorView(error: "Erro a obter perfil");
+              )
+          ],
+        )
+      )
+      : const ErrorView(error: "Erro a obter perfil");
       },
     );
   }
