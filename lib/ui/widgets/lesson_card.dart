1d427999755abd2adbabe48c1989d54491e6eb4c
Pedro Cunha <pedro10cunha@gmail.com>
2023-09-17 21:07:37 +0100

    Added scuffed progress bar with time inside it

--- lib/ui/widgets/lesson_card.dart
+++ lib/ui/widgets/lesson_card.dart
@@ -20,6 +20,9 @@ class LessonCard<T> extends StatelessWidget {
     DateTime end = DateTime.parse(lesson.data_hora_fim);
     var formatter = DateFormat('Hm');
 
+    // Math for time in class
+    // double progress = (DateTime.now().millisecondsSinceEpoch - start.millisecondsSinceEpoch) / (end.millisecondsSinceEpoch - start.millisecondsSinceEpoch);
+
     return Card(
       margin: const EdgeInsets.fromLTRB(12, 6, 12, 6),
       child: Row(
@@ -42,26 +45,47 @@ class LessonCard<T> extends StatelessWidget {
                         Text("Sala: ${lesson.sala}",
                             style: const TextStyle(
                                 fontSize: 12, fontWeight: FontWeight.w300)
-                        )
+                        ),
                       ]
                   )
               )
           ),
-          Card(
-              margin: const EdgeInsets.all(0),
-              elevation: 0,
-              color: Theme.of(context).colorScheme.primaryContainer,
-              child: Padding(
-                padding: const EdgeInsets.fromLTRB(20, 16, 20, 16),
-                child: Column(
+          Row(
+              children: [
+                //progress bar with text inside it
+                ClipRRect(
+                  borderRadius: BorderRadius.circular(10.0),
+
+                  //from https://stackoverflow.com/questions/58518161/flutter-text-inside-linearprogressindicator
+
+                  child: Stack(     //Position absolute thing for multiple things inside one another
                     children: [
-                      Text(formatter.format(start)),
-                      const Text("|"),
-                      Text(formatter.format(end)),
-                    ]
-                ),
-              )
-          ),
+                      SizedBox(
+                        height: 95,
+                        width: 80,
+                        child: LinearProgressIndicator(
+                          value: 0.6,
+                          backgroundColor: Theme.of(context).colorScheme.surfaceVariant,
+                          valueColor: AlwaysStoppedAnimation<Color>(Theme.of(context).colorScheme.primaryContainer)
+                        ),
+                      ),
+                      SizedBox(
+                        height: 95,
+                        width: 80,
+                        child: Column(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              Text(formatter.format(start)),
+                              const Text("|"),
+                              Text(formatter.format(end)),
+                            ]
+                        ),
+                      )
+                    ],
+                  ),
+                )
+              ]
+          )
         ],
       ),
     );
