985695c3fd4820a390a8d70023e350bcc7a606ef
João Alves <joao.2003.couto@gmail.com>
2023-09-20 01:47:03 +0100

    Committed example card on accident oops

--- lib/ui/widgets/schedule_tab.dart
+++ lib/ui/widgets/schedule_tab.dart
@@ -35,8 +35,6 @@ class ScheduleTab<T> extends StatelessWidget {
                 }
               }
 
-              todaySchedule.add(schedule[0]);
-
               if (todaySchedule.isEmpty) {
                 return const Center(
                   child: Column(
