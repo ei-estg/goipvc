ba8f1d00f4a205a4212978ba2be099c170a41d88
João Alves <joao.2003.couto@gmail.com>
2023-09-21 23:37:16 +0100

    School map view (Closes #22)

---
+++ lib/ui/floors.dart
@@ -0,0 +1,12 @@
+final schoolMaps = {
+  "ESA": [],
+  "ESCE": [],
+  "ESDL": [],
+  "ESE": [],
+  "ESS": [],
+  "ESTG": [
+    "assets/floors/estg/floor1.png",
+    "assets/floors/estg/floor2.png",
+    "assets/floors/estg/floor3.png",
+  ]
+};
--- lib/ui/views/school_map.dart
+++ lib/ui/views/school_map.dart
@@ -1,11 +1,43 @@
 import 'package:flutter/material.dart';
-import 'package:goipvc/ui/views/coming_soon.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:goipvc/models/myipvc/user.dart';
+import 'package:goipvc/providers/profile_provider.dart';
+import 'package:goipvc/ui/floors.dart';
+import 'package:goipvc/ui/views/error.dart';
+import 'package:goipvc/ui/views/info.dart';
+import 'package:photo_view/photo_view.dart';
 
-class SchoolMapView extends StatelessWidget {
+class SchoolMapView extends ConsumerWidget {
   const SchoolMapView({super.key});
 
+  void _openImage(BuildContext context, String img) {
+    Navigator.of(context).push(MaterialPageRoute(
+      builder: (context) => PhotoView(
+        imageProvider: AssetImage(img),
+      )
+    ));
+  }
+
   @override
-  Widget build(BuildContext context) {
-    return const ComingSoonView();
+  Widget build(BuildContext context, WidgetRef ref) {
+    MyIPVCUser? profile = ref.watch(profileProvider);
+
+    if(profile == null) return const ErrorView(error: "Erro a obter escola");
+
+    if(schoolMaps[profile.unidade_organica]!.isEmpty){
+      return const InfoView(message: "Não existem plantas para a sua escola");
+    }
+
+    return ListView(
+      children: [
+        for(var map in schoolMaps[profile.unidade_organica]!)
+          ListTile(
+            title: Image.asset(map),
+            onTap: () {
+              _openImage(context, map);
+            },
+          )
+      ],
+    );
   }
 }
