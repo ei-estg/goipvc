9a97378111a845d46dd16d87d236b21b90f94404
João Alves <joao.2003.couto@gmail.com>
2023-09-22 22:30:02 +0100

    Fixed update notifier

--- lib/ui/views/init.dart
+++ lib/ui/views/init.dart
@@ -1,8 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:goipvc/services/github.dart';
 import 'package:goipvc/services/myipvc_api.dart';
 import 'package:goipvc/ui/views/login.dart';
-import 'package:url_launcher/url_launcher.dart';
 
 import 'index.dart';
 
@@ -17,30 +15,8 @@ class _InitViewState extends State<InitView> {
   @override
   void initState() {
     super.initState();
-    getNewRelease().then((version) => {
-      if(version != null){
-        ScaffoldMessenger.of(context).showSnackBar(
-          SnackBar(
-            content: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Text("Nova versão disponível: $version"),
-                TextButton(
-                    onPressed: () {
-                      launchUrl(Uri.parse("https://github.com/joaoalves03/goipvc/releases/latest"));
-                    },
-                    child: const Text("Transferir")
-                )
-              ],
-            ),
-            duration: const Duration(seconds: 5), // Adjust the duration as needed
-          ),
-        )
-      }
-    });
-
     MyIPVCAPI().verifyAuth().then((data) {
-      Navigator.push(
+      Navigator.pushReplacement(
           context,
           data // If user is authenticated (token exists)
             ? MaterialPageRoute(builder: (context) => const IndexView())
