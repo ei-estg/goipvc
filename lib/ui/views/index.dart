1bf9ba81c4a727ff42aa56842caecc4668c645ea
João Alves <joao.2003.couto@gmail.com>
2023-09-27 18:38:15 +0100

    Fixed small mistake when rebasing

--- lib/ui/views/index.dart
+++ lib/ui/views/index.dart
@@ -3,6 +3,7 @@ import 'dart:io';
 import 'package:flutter/material.dart';
 
 import 'package:goipvc/ui/views/home.dart';
+import 'package:goipvc/ui/views/meals.dart';
 import 'package:goipvc/ui/views/menu.dart';
 import 'package:goipvc/ui/views/schedule.dart';
 import 'package:goipvc/ui/views/school_map.dart';
@@ -79,6 +80,7 @@ class _IndexViewState extends State<IndexView> {
       const HomeView(),
       const ScheduleView(),
       const SchoolMapView(),
+      const MealsView(),
       const MenuView()
     ];
 
