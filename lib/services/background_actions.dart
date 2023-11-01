4f7885227790802851c245687d3ace8f43b6cd01
João Alves <joao.2003.couto@gmail.com>
2023-11-01 16:53:52 +0000

    Disabled background alert scheduling if it's disabled

--- lib/services/background_actions.dart
+++ lib/services/background_actions.dart
@@ -1,5 +1,6 @@
 import 'package:goipvc/services/myipvc_api.dart';
 import 'package:goipvc/services/notifications.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 import 'package:workmanager/workmanager.dart';
 
 @pragma('vm:entry-point')
@@ -16,6 +17,11 @@ void callbackDispatcher() {
 }
 
 Future<void> _scheduleLessonAlerts() async {
+  SharedPreferences prefs = await SharedPreferences.getInstance();
+  if((prefs.getInt("lessonAlert") ?? 0) == 0) {
+    return;
+  }
+
   await Notifications.discardLessonWarningNotifications();
   final schedule = await MyIPVCAPI.getSchedule();
   await Notifications.parseSchedule(schedule);
