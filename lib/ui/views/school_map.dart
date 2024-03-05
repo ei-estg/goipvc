414016bdf40e53dcee5871e119784dfbc4eaf7fd
João Ferreira <j.pedro.ferreira@ipvc.pt>
2024-03-05 10:41:56 +0000

    feat: add floor selector
    + Adding floor selector to the school view map.
    +- Changing photo viewer title to "A visualizar a planta da escola" to make it more acessible in knowing what we're doing.

--- lib/ui/views/school_map.dart
+++ lib/ui/views/school_map.dart
@@ -7,73 +7,36 @@ import 'package:goipvc/providers/settings_provider.dart';
 import 'package:goipvc/ui/floors.dart';
 import 'package:goipvc/ui/views/error.dart';
 import 'package:goipvc/ui/views/info.dart';
+import 'package:goipvc/ui/widgets/floor_selector.dart';
 import 'package:photo_view/photo_view.dart';
 
 class SchoolMapView extends ConsumerWidget {
   const SchoolMapView({super.key});
 
-  void _openImage(BuildContext context, String img) {
-    Navigator.of(context).push(MaterialPageRoute(
-        builder: (context) => Scaffold(
-          appBar: AppBar(title: const Text("Piso")),
-          body: Hero(
-            tag: img,
-            child: PhotoView(
-              imageProvider: AssetImage(img),
-              backgroundDecoration: BoxDecoration(
-                  color: Theme.of(context).colorScheme.surface
-              ),
-            ),
-          ),
-        )));
-  }
-
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    MyIPVCUser? profile = ref.watch(profileProvider);
-    final settings = ref.watch(settingsProvider);
-    final systemTheme =
-        SchedulerBinding.instance.platformDispatcher.platformBrightness;
-    var theme = "";
-    final deviceWidth = MediaQuery.of(context).size.width;
+      MyIPVCUser? profile = ref.watch(profileProvider);
+      final settings = ref.watch(settingsProvider);
+      final systemTheme =
+          SchedulerBinding.instance.platformDispatcher.platformBrightness;
+      var theme = "";
 
-    if (settings.theme == "system") {
-      if (systemTheme == Brightness.dark) {
-        theme = "dark";
-      } else {
-        theme = "light";
-      }
-    } else {
-      theme = settings.theme;
-    }
-
-    if (profile == null) return const ErrorView(error: "Erro a obter escola");
-
-    if (schoolMaps[profile.unidadeOrganica]!.isEmpty) {
-      return const InfoView(message: "Não existem plantas para a sua escola");
-    }
-
-    return GridView.builder(
-        shrinkWrap: true,
-        padding: const EdgeInsets.all(8),
-        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-            crossAxisCount: (deviceWidth / 500).round(),
-            mainAxisSpacing: 4.0,
-            crossAxisSpacing: 4.0,
-            childAspectRatio: 1.57
-        ),
-        itemCount: schoolMaps[profile.unidadeOrganica]!.length,
-        itemBuilder: (BuildContext context, int index) {
-          return GestureDetector(
-            onTap: () {
-              _openImage(context, schoolMaps[profile.unidadeOrganica]![index][theme]);
-            },
-            child: Hero(
-                tag: schoolMaps[profile.unidadeOrganica]![index][theme],
-                child: Image.asset(schoolMaps[profile.unidadeOrganica]![index][theme])
-            ),
-          );
+      if (settings.theme == "system") {
+        if (systemTheme == Brightness.dark) {
+          theme = "dark";
+        } else {
+          theme = "light";
         }
-    );
-  }
+      } else {
+        theme = settings.theme;
+      }
+
+      if (profile == null) return const ErrorView(error: "Erro a obter escola");
+
+      if (schoolMaps[profile.unidadeOrganica]!.isEmpty) {
+        return const InfoView(message: "Não existem plantas para a sua escola");
+      }
+
+      return FloorSelector(profile: profile, theme: theme);
+    }
 }
