143d2635467c36378772877b8c61467b76bcee48
João Ferreira <j.pedro.ferreira@ipvc.pt>
2024-03-05 15:08:25 +0000

    add: floor info and caption
    Adding floor name to the floors list and also caption for the blueprint.

--- lib/ui/floors.dart
+++ lib/ui/floors.dart
@@ -7,15 +7,41 @@ final schoolMaps = {
   "ESTG": [
     {
       "light": "assets/floors/estg/light1.png",
-      "dark": "assets/floors/estg/dark1.png"
+      "dark": "assets/floors/estg/dark1.png",
+      "name": "Piso 1",
+      "caption": {
+        "SE - Saída de Emergência",
+        "A - Anfiteatro",
+        "S - Sala de Aula",
+        "AE - Associação de Estudantes",
+        "AR - Armazém de Reagentes",
+        "L - Laboratório",
+        "G - Gabinete",
+        "SA - Serviços Académicos"
+      }
     },
     {
       "light": "assets/floors/estg/light2.png",
-      "dark": "assets/floors/estg/dark2.png"
+      "dark": "assets/floors/estg/dark2.png",
+      "name": "Piso 2",
+      "caption": {
+        "SE - Saída de Emergência",
+        "A - Anfiteatro",
+        "S - Sala de Aula",
+        "L - Laboratório",
+        "G - Gabinete",
+        "SI - Serviços Informáticos"
+      }
     },
     {
       "light": "assets/floors/estg/light3.png",
-      "dark": "assets/floors/estg/dark3.png"
+      "dark": "assets/floors/estg/dark3.png",
+      "name": "Piso 3",
+      "caption": {
+        "S - Sala de Aula",
+        "L - Laboratório",
+        "G - Gabinete",
+      }
     },
   ]
 };
