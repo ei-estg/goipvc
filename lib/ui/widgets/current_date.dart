c55f48499af751e727741d5fd11c9b33a6ac57ac
João Alves <joao.2003.couto@gmail.com>
2024-03-14 11:44:11 +0000

    fix(redesign) forgot to add the actual date

--- lib/ui/widgets/current_date.dart
+++ lib/ui/widgets/current_date.dart
@@ -1,26 +1,29 @@
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 
 class CurrentDate extends StatelessWidget {
   const CurrentDate({super.key});
 
   @override
   Widget build(BuildContext context) {
+    DateTime now = DateTime.now();
+
     return Row(
       children: [
-        const Column(
+        Column(
           crossAxisAlignment: CrossAxisAlignment.end,
           children: [
             Text(
-              "Quinta-feira",
-              style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+              DateFormat("EEEE", "pt-PT").format(now),
+              style: const TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
             ),
-            Text("Março 2024")
+            Text(DateFormat("MMMM yyyy", "pt-PT").format(now))
           ],
         ),
         Padding(
           padding: const EdgeInsets.only(left: 8),
           child: Text(
-            "17",
+            now.day.toString(),
             style: TextStyle(
                 fontSize: 50,
                 fontWeight: FontWeight.bold,
